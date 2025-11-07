394bf9ff3bd1f5fe7d8ec9bc732506b4410f88f6
AdvayRChandra <advay.chandra@protonmail.com>
2025-11-07 09:46:40 -0500

    Add flow handling to Connector class and implement Pipe, Bend, and Valve classes

--- systems/connectors.py
+++ systems/connectors.py
@@ -6,3 +6,38 @@ class Connector:
         self.inputFlow = inputFlow
         self.outputFlow = outputFlow
         self.connectorFunction = connectorFunction
+    
+    def pass_flow(self, **kwargs):
+        input_composition = kwargs.get("input_composition", dict())
+        total_flow = kwargs.get("total_flow", None)
+        return self.connectorFunction(input)
+
+class Pipe(Connector):
+    def __init__(self):
+        super().__init__("Pipe Input Flow", "Pipe Output Flow", self.pipe_function)
+        # Additional initialization for Pipe can go here
+
+    
+    def pipe_function(self, input=dict()):
+        return input
+        # pass
+
+class Bend(Connector):
+    def __init__(self):
+        super().__init__("Bend Input Flow", "Bend Output Flow", self.bend_function)
+        # Additional initialization for Bend can go here
+
+    
+    def bend_function(self, input=dict()):
+        return input
+        # pass
+
+class Valve(Connector):
+    def __init__(self):
+        super().__init__("Valve Input Flow", "Valve Output Flow", self.valve_function)
+        # Additional initialization for Valve can go here
+
+    
+    def valve_function(self, input=dict()):
+        return input
+        # pass
